8de253d84275cd68de6114d5ac8d14de9f1d6a48
Gui-ASUS <guifernandespro@gmail.com>
2023-01-06 23:17:31 +0100

    salving advancements carousel

--- src/App.vue
+++ src/App.vue
@@ -1,120 +1,120 @@
 <template>
-  <main>
-    <section>
-      <the-nav></the-nav>
-    </section>
+	<main>
+		<section>
+			<the-nav></the-nav>
+		</section>
 
-    <parallax imageUrl="/img/icon-team-picture.jpg" />
+		<parallax imageUrl="/img/icon-team-picture.jpg" />
 
-    <div class="row">
-      <section>
-        <logo-cards></logo-cards>
-        <introduction-section></introduction-section>
-        <benefits-cards></benefits-cards>
-      </section>
+		<div class="row">
+			<section>
+				<logo-cards></logo-cards>
+				<introduction-section></introduction-section>
+				<benefits-cards></benefits-cards>
+			</section>
 
-      <section>
-        <head-coach-section v-if="!isMobile"></head-coach-section>
-        <mobile-head-coach v-else></mobile-head-coach>
-      </section>
+			<section>
+				<head-coach-section v-if="!isMobile"></head-coach-section>
+				<mobile-head-coach v-else></mobile-head-coach>
+			</section>
 
-      <parallax imageUrl="/img/bjj-training-brussels_orig.jpg" />
+			<parallax imageUrl="/img/bjj-training-brussels_orig.jpg" />
 
-      <grid-helper :id="'location'">
-        <location-section></location-section>
-      </grid-helper>
+			<grid-helper :id="'location'">
+				<location-section></location-section>
+			</grid-helper>
 
-      <grid-helper>
-        <gym-section></gym-section>
-      </grid-helper>
+			<grid-helper>
+				<gym-section></gym-section>
+			</grid-helper>
 
-      <parallax imageUrl="/img/seminar-2018.jpg" />
+			<parallax imageUrl="/img/seminar-2018.jpg" />
 
-      <grid-helper :id="'time-table'">
-        <adults-table></adults-table>
-      </grid-helper>
+			<grid-helper :id="'time-table'">
+				<adults-table></adults-table>
+			</grid-helper>
 
-      <grid-helper :id="'kids-time-table'">
-        <kids-table></kids-table>
-      </grid-helper>
+			<grid-helper :id="'kids-time-table'">
+				<kids-table></kids-table>
+			</grid-helper>
 
-      <grid-helper :id="'fees'">
-        <fee-section></fee-section>
-      </grid-helper>
+			<grid-helper :id="'fees'">
+				<fee-section></fee-section>
+			</grid-helper>
 
-      <parallax imageUrl="/img/icon-team-precorona.jpg" />
+			<parallax imageUrl="/img/icon-team-precorona.jpg" />
 
-      <grid-helper :id="'faq'">
-        <faq-section></faq-section>
-      </grid-helper>
-      <!-- End -->
-    </div>
-    <!-- footer with contact info -->
-    <contact-info-section></contact-info-section>
-  </main>
+			<grid-helper :id="'faq'">
+				<faq-section></faq-section>
+			</grid-helper>
+			<!-- End -->
+		</div>
+		<!-- footer with contact info -->
+		<contact-info-section></contact-info-section>
+	</main>
 
-  <!-- sidenav for mobile -->
-  <side-nav></side-nav>
+	<!-- sidenav for mobile -->
+	<side-nav></side-nav>
 </template>
 
 <script>
-import TheNav from "./components/TheNav.vue";
-import Parallax from "./components/Parallax.vue";
+	import TheNav from "./components/TheNav.vue";
+	import Parallax from "./components/Parallax.vue";
 
-import SideNav from "./components/mobile/SideNav.vue";
-import MobileHeadCoach from "./components/mobile/MobileHeadCoach.vue";
+	import SideNav from "./components/mobile/SideNav.vue";
+	import MobileHeadCoach from "./components/mobile/MobileHeadCoach.vue";
 
-import LogoCards from "./components/layout/LogoCards.vue";
-import BenefitsCards from "./components/layout/BenefitsCards.vue";
-import KidsTable from "./components/layout/KidsTable.vue";
-import AdultsTable from "./components/layout/AdultsTable.vue";
+	import LogoCards from "./components/layout/LogoCards.vue";
+	import BenefitsCards from "./components/layout/BenefitsCards.vue";
+	import KidsTable from "./components/layout/KidsTable.vue";
+	import AdultsTable from "./components/layout/AdultsTable.vue";
 
-import IntroductionSection from "./components/sections/IntroductionSection.vue";
-import FeeSection from "./components/sections/FeeSection.vue";
-import FaqSection from "./components/sections/FaqSection.vue";
-import HeadCoachSection from "./components/sections/HeadCoachSection.vue";
-import ContactInfoSection from "./components/sections/ContactInfo.vue";
-import GymSection from "./components/sections/GymSection.vue";
-import LocationSection from "./components/sections/LocationSection.vue";
+	import IntroductionSection from "./components/sections/IntroductionSection.vue";
+	import FeeSection from "./components/sections/FeeSection.vue";
+	import FaqSection from "./components/sections/FaqSection.vue";
+	import HeadCoachSection from "./components/sections/HeadCoachSection.vue";
+	import ContactInfoSection from "./components/sections/ContactInfo.vue";
+	import GymSection from "./components/sections/GymSection.vue";
+	import LocationSection from "./components/sections/LocationSection.vue";
 
-import GridHelper from "./components/util/GridHelperScroll.vue";
+	import GridHelper from "./components/util/GridHelperScroll.vue";
 
-export default {
-  components: {
-    TheNav,
-    Parallax,
-    SideNav,
-    LogoCards,
-    KidsTable,
-    AdultsTable,
-    FeeSection,
-    FaqSection,
-    HeadCoachSection,
-    MobileHeadCoach,
-    ContactInfoSection,
-    BenefitsCards,
-    GymSection,
-    IntroductionSection,
-    LocationSection,
-    GridHelper,
-  },
-  data() {
-    return {
-      isMobile: this.$store.getters.getMobile,
-    };
-  },
-  beforeCreate() {
-    this.$store.dispatch("checkMobile");
-  },
-};
+	export default {
+		components: {
+			TheNav,
+			Parallax,
+			SideNav,
+			LogoCards,
+			KidsTable,
+			AdultsTable,
+			FeeSection,
+			FaqSection,
+			HeadCoachSection,
+			MobileHeadCoach,
+			ContactInfoSection,
+			BenefitsCards,
+			GymSection,
+			IntroductionSection,
+			LocationSection,
+			GridHelper,
+		},
+		data() {
+			return {
+				isMobile: this.$store.getters.getMobile,
+			};
+		},
+		beforeCreate() {
+			this.$store.dispatch("checkMobile");
+		},
+	};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Russo+One&display=swap");
+	@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");
+	@import url("https://fonts.googleapis.com/css2?family=Russo+One&display=swap");
 
-* {
-  font-family: "Barlow Semi Condensed", sans-serif;
-  box-sizing: border-box;
-}
+	* {
+		font-family: "Barlow Semi Condensed", sans-serif;
+		box-sizing: border-box;
+	}
 </style>
--- src/components/sections/HeadCoachSection.vue
+++ src/components/sections/HeadCoachSection.vue
@@ -1,55 +1,56 @@
 <template>
-  <section>
-    <standard-header> Our Coaches </standard-header>
+	<section>
+		<standard-header> Our Coaches </standard-header>
 
-    <section id="carousel-container">
-      <div class="carousel" data-carousel>
-        <button class="carousel-button prev" data-carousel-button="prev">
-          <ion-icon name="chevron-back-outline"></ion-icon>
-        </button>
-        <button class="carousel-button next" data-carousel-button="next">
-          <ion-icon name="chevron-forward-outline"></ion-icon>
-        </button>
-        <ul data-slides>
-          <li class="slide pablo" data-active>
-            <img src="../../../public/img/pablo_pic.jpg" />
-            <div class="backdrop"></div>
-            <h1>Pablo Cerino</h1>
-            <p>
-              Our main coach is Pablo Cerino, He holds a 4th degree black belt
-              in Brazilian Jiu Jitsu and has been practicing BJJ since 1993.
-              Pablo actively competes in the biggest competitions and achieve
-              great results. Pablo is a Brazilian native but has lived in
-              Brussels since 2008 and speaks Portugese, French and English
-              fluently.
-            </p>
-          </li>
-          <li class="slide gui">
-            <img src="../../../public/img/gui_medals_pic.jpg" />
-            <div class="backdrop"></div>
-            <h1>Gui Fernandes</h1>
-            <p>
-              Gui is a high level purple belt competitor from our team. He has
-              been achieving great results in competitions around Europe. He
-              also is a part of our coaching staff in our dojo.
-            </p>
-            <span>Competition in Berlin, Germany 2021</span>
-          </li>
-          <!-- <li class="slide max">
-            <img src="../../../public/img/max_picture.jpg" />
-            <div class="backdrop"></div>
-            <h1>Max Koning</h1>
-            <p>
-              Gui is a high level purple belt competitor from our team. He has
-              been achieving great results in competitions around Europe. He
-              also is a part of our coaching staff in our dojo.
-            </p>
-          </li> -->
-        </ul>
-      </div>
-    </section>
+		<section id="carousel-container">
+			<div class="carousel" data-carousel>
+				<button class="carousel-button prev" data-carousel-button="prev">
+					<ion-icon name="chevron-back-outline"></ion-icon>
+				</button>
+				<button class="carousel-button next" data-carousel-button="next">
+					<ion-icon name="chevron-forward-outline"></ion-icon>
+				</button>
+				<ul data-slides>
+					<li class="slide pablo" data-active>
+						<img src="../../../public/img/pablo_pic.jpg" />
+						<div class="backdrop"></div>
+						<h1>Pablo Cerino</h1>
+						<p>
+							Our main coach is Pablo Cerino, He holds a 4th degree black belt
+							in Brazilian Jiu Jitsu and has been practicing BJJ since 1993.
+							Pablo actively competes in the biggest competitions and achieve
+							great results. Pablo is a Brazilian native but has lived in
+							Brussels since 2008 and speaks Portugese, French and English
+							fluently.
+						</p>
+					</li>
+					<li class="slide gui">
+						<img src="../../../public/img/gui_ajpBrussels_doubleGold.jpg" />
+						<div class="backdrop"></div>
+						<h1>Gui Fernandes</h1>
+						<p>
+							Gui is a high level purple belt competitor from our team. He has
+							been achieving great results in competitions around Europe. He
+							also is a part of our coaching staff in our dojo.
+						</p>
+						<span>AJP Brussels Competition 2022</span>
+					</li>
+					<li class="slide max">
+						<img src="../../../public/img/max_picture.jpg" />
+						<div class="backdrop"></div>
+						<h1>Max Koning</h1>
+						<p>
+							Max is a high level purple belt from our team. He has
+							been achieving great results in competitions around Europe. He
+							also is a part of our coaching staff in our dojo.
+						</p>
+            <span>Competition in Flanders, Belgium 2021</span>
+					</li>
+				</ul>
+			</div>
+		</section>
 
-    <!-- <section id="desc">
+		<!-- <section id="desc">
       <div class="new-container">
         <div id="paragraphs">
           <p :class="pClass">
@@ -75,20 +76,20 @@
         />
       </div>
     </section> -->
-  </section>
+	</section>
 </template>
 
 <script>
 import StandardHeader from "../util/StandardHeader.vue";
 export default {
-  computed: {
-    pClass() {
-      return "grey-text text-darken-4";
-    },
-  },
-  components: {
-    StandardHeader,
-  },
+	computed: {
+		pClass() {
+			return "grey-text text-darken-4";
+		},
+	},
+	components: {
+		StandardHeader,
+	},
 };
 </script>
 
@@ -98,162 +99,195 @@ export default {
   margin: 5px;
 } */
 #paragraphs {
-  margin: 2rem;
-  flex: 2;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  justify-content: center;
-  max-width: 30vw;
-  text-align: justify;
+	margin: 2rem;
+	flex: 2;
+	display: flex;
+	flex-direction: column;
+	text-align: center;
+	justify-content: center;
+	max-width: 30vw;
+	text-align: justify;
 }
 
 #desc {
-  margin: 0 10vw;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	margin: 0 10vw;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
 .new-container {
-  width: calc(100% - 10vw);
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	width: calc(100% - 10vw);
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
 section {
-  margin: 4rem 0;
+	margin: 4rem 0;
 }
 </style>
 
 <style scoped>
 #carousel-container {
-  margin: auto;
-  width: 75vw;
-  min-height: 100vh;
-  user-select: none;
+	margin: auto;
+	width: 75vw;
+	min-height: 100vh;
+	user-select: none;
 }
 
 .carousel {
-  width: 100%;
-  height: 50vw;
-  position: relative;
-  background: 50% 50% / cover;
+	width: 100%;
+	height: 50vw;
+	position: relative;
+	background: 50% 50% / cover;
 }
 
 .carousel ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+	list-style: none;
+	margin: 0;
+	padding: 0;
 }
 
 .slide {
-  position: absolute;
-  inset: 0;
-  opacity: 0;
-  transition: 500ms opacity ease-in-out;
-  transition-delay: 200ms;
+	position: absolute;
+	inset: 0;
+	opacity: 0;
+	transition: 500ms opacity ease-in-out;
+	transition-delay: 200ms;
 }
 
 .slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-  object-fit: cover;
-  object-position: center;
-  z-index: 1;
+	display: block;
+	width: 100%;
+	height: 100%;
+	border-radius: 10px;
+	object-fit: cover;
+	object-position: center;
+	z-index: 1;
 }
 
 .slide[data-active] {
-  opacity: 1;
-  z-index: 1;
-  transition-delay: 0ms;
+	opacity: 1;
+	z-index: 1;
+	transition-delay: 0ms;
 }
 
 .carousel-button {
-  position: absolute;
-  z-index: 3;
-  top: 50%;
-  transform: translateY(-50%);
-  font-size: 4rem;
-  background: none;
-  border: none;
-  color: rgba(255, 255, 255, 0.589);
-  cursor: pointer;
+	position: absolute;
+	z-index: 3;
+	top: 50%;
+	transform: translateY(-50%);
+	font-size: 4rem;
+	background: none;
+	border: none;
+	color: rgba(255, 255, 255, 0.589);
+	cursor: pointer;
 }
 
 .carousel-button:hover,
 .carousel-button:focus {
-  color: rgb(255, 255, 255);
+	color: rgb(255, 255, 255);
 }
 
 .carousel-button.prev {
-  left: 1rem;
+	left: 1rem;
 }
 
 .carousel-button.next {
-  right: 1rem;
+	right: 1rem;
 }
 </style>
 
 <style scoped>
+img {
+  z-index: 0;
+  /* position: relative; */
+}
 .backdrop {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 100%;
-  width: 100%;
-  background-color: #000;
-  border-radius: 10px;
-  z-index: 33;
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	height: 100%;
+	width: 100%;
+	background-color: rgba(0, 0, 0, 0.274);
+	border-radius: 10px;
+	z-index: 333;
 }
 
 .slide.pablo p {
-  font-family: "Oswald", sans-serif;
-  position: absolute;
-  top: 51%;
-  transform: translateY(-50%);
-  left: 6rem;
-  width: 50%;
+	font-family: "Oswald", sans-serif;
+	position: absolute;
+	top: 51%;
+	transform: translateY(-50%);
+	left: 6rem;
+	width: 50%;
 }
 
 .slide.pablo h1 {
-  font-family: "Poppins", sans-serif;
-  font-weight: bold;
-  position: absolute;
-  top: 28%;
-  left: 6rem;
+	font-family: "Poppins", sans-serif;
+	font-weight: bold;
+	position: absolute;
+	top: 28%;
+	left: 6rem;
 }
 
 .slide.gui h1 {
-  font-family: "Poppins", sans-serif;
-  font-weight: 600;
-  font-size: 3rem;
-  position: absolute;
-  top: 30%;
-  left: 6rem;
+	font-family: "Poppins", sans-serif;
+	font-weight: 600;
+	font-size: 3rem;
+	position: absolute;
+	color: #fff;
+	top: 30%;
+	left: 6rem;
 }
 
+.slide.max h1 {
+	font-family: "Poppins", sans-serif;
+	font-weight: 600;
+	font-size: 3rem;
+	position: absolute;
+	color: #000;
+	top: 30%;
+	left: 6rem;
+}
+
+.slide.gui img {
+	width: 100%;
+}
 .slide.gui p {
-  font-family: "Oswald", sans-serif;
-  color: #000;
-  /* font-size: 1.5rem; */
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  left: 6rem;
-  width: 35%;
+	font-family: "Oswald", sans-serif;
+	color: #fff;
+	/* font-size: 1.5rem; */
+	position: absolute;
+	top: 50%;
+	transform: translateY(-50%);
+	left: 6rem;
+	width: 35%;
 }
-
+.slide.max p {
+	font-family: "Oswald", sans-serif;
+	color: #000;
+	/* font-size: 1.5rem; */
+	position: absolute;
+	top: 50%;
+	transform: translateY(-50%);
+	left: 6rem;
+	width: 35%;
+}
 .slide.gui span {
-  position: absolute;
-  bottom: 1rem;
-  right: 1rem;
-  color: #fff;
+	position: absolute;
+	bottom: 1rem;
+	right: 1rem;
+	color: #fff;
+}
+.slide.max span {
+	position: absolute;
+	bottom: 1rem;
+	right: 1rem;
+	color: #fff;
 }
 </style>
